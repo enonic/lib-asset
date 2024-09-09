15eade2c7838e676c1857d57f502c6bc0bd3dfb8
Christian Westgaard <comlock@gmail.com>
2024-09-09 14:29:30 +0200

    Make getFingerprint work in java

--- src/main/java/com/enonic/lib/asset/AppHelper.java
+++ src/main/java/com/enonic/lib/asset/AppHelper.java
@@ -4,20 +4,17 @@ import java.util.Objects;
 
 import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.context.ContextAccessor;
-// import com.enonic.xp.core.impl.app.ApplicationFactoryService;
-// import com.enonic.xp.core.impl.app.resolver.ApplicationUrlResolver;
-// import com.enonic.xp.core.internal.HexCoder;
 import com.enonic.lib.asset.HexCoder;
 import com.enonic.xp.resource.Resource;
 import com.enonic.xp.resource.ResourceKey;
-// import com.enonic.xp.resource.ResourceService;
+import com.enonic.xp.resource.ResourceService;
+import com.enonic.xp.script.bean.BeanContext;
+import com.enonic.xp.script.bean.ScriptBean;
 import com.enonic.xp.server.RunMode;
 
-public class AppHelper
+public class AppHelper implements ScriptBean
 {
-  // protected ResourceService resourceService;
-
-  // private final ApplicationFactoryService applicationFactoryService;
+  protected ResourceService resourceService;
 
   public boolean isDevMode()
   {
@@ -25,17 +22,20 @@ public class AppHelper
   }
 
   public String getFingerprint(String application) {
-    // final ApplicationKey applicationKey = ApplicationKey.from( application );
-    // new ApplicationResolver().portalRequest( this.portalRequest ).application( this.params.getApplication() ).resolve();
+    final ApplicationKey applicationKey = ApplicationKey.from( application );
 
-    // final Resource resource = this.resourceService.getResource( ResourceKey.from( applicationKey, "META-INF/MANIFEST.MF" ) );
-    // final Resource resource = getResource( ResourceKey.from( applicationKey, "META-INF/MANIFEST.MF" ) );
-    // if ( !resource.exists() )
-    // {
-    //   throw new IllegalArgumentException( "Could not find application [" + applicationKey + "]" );
-    // }
-    // return isDevMode() ? String.valueOf(stableTime()) : HexCoder.toHex(resource.getTimestamp());
-    return isDevMode() ? String.valueOf(stableTime()) : "1234567890123456";
+    final Resource resource = this.resourceService.getResource( ResourceKey.from( applicationKey, "META-INF/MANIFEST.MF" ) );
+    if ( !resource.exists() )
+    {
+      throw new IllegalArgumentException( "Could not find application [" + applicationKey + "]" );
+    }
+    return isDevMode() ? String.valueOf(stableTime()) : HexCoder.toHex(resource.getTimestamp());
+  }
+
+  @Override
+  public void initialize( final BeanContext context )
+  {
+      this.resourceService = context.getService( ResourceService.class ).get();
   }
 
   private static long stableTime()
@@ -43,16 +43,4 @@ public class AppHelper
     final Long localScopeTime = (Long) ContextAccessor.current().getLocalScope().getAttribute( "__currentTimeMillis" );
     return Objects.requireNonNullElseGet( localScopeTime, System::currentTimeMillis );
   }
-
-  // private Resource getResource( final ResourceKey key )
-  // {
-  //   return findApplicationUrlResolver( key.getApplicationKey() ).map( urlResolver -> urlResolver.findResource( key.getPath() ) )
-  //     .orElse( new UrlResource( key, null ) );
-  // }
-
-  // private Optional<ApplicationUrlResolver> findApplicationUrlResolver( final ApplicationKey key )
-  // {
-  //   final String resolverSource = (String) ContextAccessor.current().getAttribute( ResourceConstants.RESOURCE_SOURCE_ATTRIBUTE );
-  //   return applicationFactoryService.findResolver( isSystemApp( key ) ? SYSTEM_APPLICATION_KEY : key, resolverSource );
-  // }
 }
