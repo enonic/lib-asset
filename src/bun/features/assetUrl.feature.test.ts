8e413425fbf695aeab5655116796bf1c16da6bba
Christian Westgaard <comlock@gmail.com>
2024-09-16 14:11:06 +0200

    Fix #13 Remove application parameter

--- src/bun/features/assetUrl.feature.test.ts
+++ src/bun/features/assetUrl.feature.test.ts
@@ -75,9 +75,6 @@ export const steps: StepDefinitions = ({ given, and, when, then }) => {
     assetUrlParams = {};
     // log.debug('table:%s', table);
     table.forEach(({ parameter, value }) => {
-      // if (parameter === 'application') {
-      //   assetUrlParams.application = value as string;
-      // } else
       if (parameter === 'params') {
         assetUrlParams.params = JSON.parse(value as string) as Record<string, unknown>;
       } else if (parameter === 'path') {
--- src/main/resources/lib/enonic/asset/assetUrl.ts
+++ src/main/resources/lib/enonic/asset/assetUrl.ts
@@ -12,29 +12,25 @@ export interface AssetUrlParams {
   // Required
   path: string
   // Optional
-  // application?: string
   params?: object
   type?: 'server' | 'absolute'
 }
 
 export function assetUrl({
-  // application = app.name,
   params,
   path,
   type,
 }: AssetUrlParams): string {
-  const application = app.name;
   const pathWithoutTrailingSlash = path.replace(/\/$/, '');
   let assetServiceUrl = serviceUrlRootViaAssetUrl({
-    // application,
     params,
     service: 'asset',
     type,
   });
   if (isCacheBust()) {
-    const fingerprint = getFingerprint(application);
+    const fingerprint = getFingerprint(app.name);
     if (fingerprint) {
-      assetServiceUrl = assetServiceUrl.replace(`/_/service/${application}/asset`, `/_/service/${application}/asset/${fingerprint}`);
+      assetServiceUrl = assetServiceUrl.replace(`/_/service/${app.name}/asset`, `/_/service/${app.name}/asset/${fingerprint}`);
     }
   }
 
